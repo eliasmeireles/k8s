5cee7d1c07743e556613895b3b058d9b870e1ab7
Elias Miereles <eliasmflilico@gmail.com>
2024-11-16 20:08:12 -0300

    test: trying k8s features like ingress

--- learning/cluster-generate/kube-config.sh
+++ learning/cluster-generate/kube-config.sh
@@ -6,6 +6,7 @@ echo "$(hostname) Installing Kubernetes components..."
 sudo apt-get update && sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
+    gpg \
     curl \
     net-tools
 
@@ -39,9 +40,8 @@ sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/con
 sudo systemctl restart containerd
 
 # Install Kubernetes components
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
